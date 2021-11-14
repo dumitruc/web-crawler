872cc091d6537e6a02095798f4a6fb1b5b027da3
dumitruc <diakon@gmail.com>
2021-11-14 17:26:56 +0000

    Organised high level flow...

---
+++ src/main/java/com/dumitruc/training/webcrawler/LittleCrawler.java
@@ -0,0 +1,20 @@
+package com.dumitruc.training.webcrawler;
+
+public class LittleCrawler {
+
+    public static void run(String[] args){
+
+        //Initiate system
+        // // Init BASE address
+        // // Init working queues, lists, variables
+
+        // Start thread orchestration
+        // run in loop (until no more work)
+        // // Start URL Master
+        // // Start crawlies
+        // stop in-flight threads
+
+        //Print out the results
+
+    }
+}
--- src/main/java/com/dumitruc/training/webcrawler/Main.java
+++ src/main/java/com/dumitruc/training/webcrawler/Main.java
@@ -6,5 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Arrays.stream(args).forEach(System.out::println);
+
+        LittleCrawler.run(args);
     }
 }
