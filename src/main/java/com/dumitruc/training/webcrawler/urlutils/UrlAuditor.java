2352c39fd55a7258dee39cee2d1462508e62cc25
dumitruc <diakon@gmail.com>
2021-11-16 23:58:56 +0000

    No change

--- src/main/java/com/dumitruc/training/webcrawler/urlutils/UrlAuditor.java
+++ src/main/java/com/dumitruc/training/webcrawler/urlutils/UrlAuditor.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 public class UrlAuditor {
 
     private static final Logger logger = LogManager.getLogger(UrlAuditor.class);
